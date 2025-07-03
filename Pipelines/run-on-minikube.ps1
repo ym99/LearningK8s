2a3f5d37f44800e14eba8633c580b5f8b2265c86
ym991 <yuriy.myshkevych@gmail.com>
2025-07-03 17:41:54 -0400

    run-in-docker and run-on-minikube are ready

--- Pipelines/run-on-minikube.ps1
+++ Pipelines/run-on-minikube.ps1
@@ -1,4 +1,4 @@
-Pipelines\build-all.ps1
+.\Pipelines\Build\build-all.ps1
 
 if (minikube image ls)
 {
@@ -12,14 +12,8 @@ else
 minikube image load lk8s-api:latest
 minikube image load lk8s-app:latest
 
-
-Push-Location .\Pipelines
+Push-Location .\Pipelines\RunOnK8s\
 #terraform init
 terraform plan
 terraform apply -auto-approve
 Pop-Location
-
-# create
-# docker network create sample-network
-# docker run -d --network sample-network --name sample-api -p 5001:5001 sample-api
-# docker run -d --network sample-network --name sample-app -p 5002:5002 sample-app
