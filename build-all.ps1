ca03d80a375e14ce0d190e8cf997967d355f2489
ym991 <yuriy.myshkevych@gmail.com>
2025-07-03 12:01:04 -0400

    progress

--- build-all.ps1
+++
@@ -1,8 +0,0 @@
-cd .\LK8sApi
-docker build -t sample-api .
-cd ..
-
-cd .\LK8sApp
-docker build -t sample-app .
-cd ..
- 
--- run-all.ps1
+++ run-all.ps1
@@ -1,9 +1,24 @@
+# build all
+cd .\LK8sApi
+docker build -t sample-api .
+cd ..
+
+cd .\LK8sApp
+docker build -t sample-app .
+cd ..
+
+# clean docker
 if (docker ps -aq --filter "name=sample-api") {
     docker rm -f sample-api
 }
-docker run -d -p 5001:5001 --name sample-api sample-api
-
 if (docker ps -aq --filter "name=sample-app") {
     docker rm -f sample-app
 }
-docker run -d -p 5002:5002 --name sample-app sample-app
+if (docker network ls --filter "name=sample-network" --format "{{.Name}}" | Select-String "sample-network") {
+    docker network rm sample-network
+}
+
+# create
+docker network create sample-network
+docker run -d --network sample-network --name sample-api -p 5001:5001 sample-api
+docker run -d --network sample-network --name sample-app -p 5002:5002 sample-app
