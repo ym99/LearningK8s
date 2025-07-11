c12c5401f85442aee2614cd78bd262346cceef5a
ym991 <yuriy.myshkevych@gmail.com>
2025-07-11 12:55:48 -0400

    added namespaces

--- run-on-minikube.ps1
+++ run-on-minikube.ps1
@@ -8,9 +8,10 @@ function Set-Tag([string]$k8sYaml, [string]$tag)
 }
 
 $version = [System.DateTime]::Now.ToString('yyyy-MM-dd--HH-mm-ss')
-.\Pipelines\Build\build-all.ps1 -version $version -redisUrl redis-service:6379 -appUrl 'http://lk8s-app-service:5002'
+.\Pipelines\Build\build-all.ps1 -version $version -redisUrl redis-service.lk8s-data.svc.cluster.local:6379 -appUrl 'http://lk8s-app-service.lk8s-app.svc.cluster.local:5002'
 
 Push-Location .\Pipelines\RunOnK8s\
+kubectl apply -f (Set-Tag namespaces.yaml $version)
 kubectl apply -f (Set-Tag redis.yaml $version)
 kubectl apply -f (Set-Tag lk8s-app.yaml $version)
 kubectl apply -f (Set-Tag lk8s-api.yaml $version)
