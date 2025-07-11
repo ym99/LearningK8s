3e4421ef9c33fa4df16415819b350ab55dfc6a00
ym991 <yuriy.myshkevych@gmail.com>
2025-07-11 11:15:08 -0400

    replaces terraform with kubectl yaml

--- Pipelines/Build/build-all.ps1
+++ Pipelines/Build/build-all.ps1
@@ -5,12 +5,27 @@ param(
 
 )
 
+# Check if logged in to Docker Hub
+Write-Host "Checking Docker Hub authentication..."
+try {
+    docker info | Out-Null
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Please run 'docker login' first"
+        exit 1
+    }
+} catch {
+    Write-Host "Please run 'docker login' first"
+    exit 1
+}
+
 Push-Location .\LK8sApi
 docker build -t lk8s-api --build-arg APP_URL_ARG=$appUrl .
-docker tag lk8s-api lk8s-api:$version
+docker tag lk8s-api ym1999/lk8s-api:$version
+docker push ym1999/lk8s-api:$version
 Pop-Location
 
 Push-Location .\LK8sApp
 docker build -t lk8s-app --build-arg REDIS_URL_ARG=$redisUrl .
-docker tag lk8s-app lk8s-app:$version 
+docker tag lk8s-app ym1999/lk8s-app:$version 
+docker push ym1999/lk8s-app:$version
 Pop-Location
--- run-on-minikube.ps1
+++ run-on-minikube.ps1
@@ -1,27 +1,17 @@
+function Set-Tag([string]$k8sYaml, [string]$tag)
+{
+   $tmpFileName = "$($k8sYaml).tmp"
+
+   Get-Content $k8sYaml | ForEach-Object { $_ -replace '[$]{TAG}', $tag } | Set-Content $tmpFileName
+
+   $tmpFileName
+}
+
 $version = [System.DateTime]::Now.ToString('yyyy-MM-dd--HH-mm-ss')
-.\Pipelines\Build\build-all.ps1 -version $version -appUrl 'http://lk8s-app-service:5002'
-
-if (minikube image ls)
-{
-   Write-Host "Minikube is running..."
-}
-else
-{
-   minikube start
-   #minikube addons enable ingress
-}
-
-minikube image load lk8s-api:$version
-minikube image load lk8s-app:$version
+.\Pipelines\Build\build-all.ps1 -version $version -redisUrl redis-service:6379 -appUrl 'http://lk8s-app-service:5002'
 
 Push-Location .\Pipelines\RunOnK8s\
-#terraform init
-terraform plan -var="image_version=$version"
-terraform apply -var="image_version=$version" -auto-approve
+kubectl apply -f (Set-Tag redis.yaml $version)
+kubectl apply -f (Set-Tag lk8s-app.yaml $version)
+kubectl apply -f (Set-Tag lk8s-api.yaml $version)
 Pop-Location
-
-# Force deployment refresh
-kubectl rollout restart deployment lk8s-api
-kubectl rollout restart deployment lk8s-app
-
-minikube service lk8s-api-service
